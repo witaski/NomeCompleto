0c617e29b674557dd8f8fceb9943748bbce75143
Wellington Witaski <wellingtonwitaski@hotmail.com>
2021-08-31 12:18:34 -0300

    Adiciona solução do exercício NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome, sobrenome;
+            Console.Write("Digite seu nome:");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite seu sobrenome:");
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"Nome completo:{nome} {sobrenome}");
+            Console.WriteLine($"Nome de catálogo:{sobrenome.ToUpper()}, {nome}");
+
+
+        }
+    }
+}
